15ecdc161f439f2d513d0954aee689ba41b66af6
Zargham Khan <zarghamkhan980@gmail.com>
2022-01-30 21:30:53 +0100

    added new sections

--- components/Usecases.tsx
+++ components/Usecases.tsx
@@ -2,79 +2,6 @@ import * as React from "react";
 const Usecases: React.FunctionComponent = () => {
   return (
     <section className="mt-24 text-gray-600">
-      {/* <div className="flex flex-col w-full mt-20 text-center">
-        <h1 className="mb-4 text-2xl font-bold text-gray-900 sm:text-5xl title-font">
-          Some amazing use cases for you
-        </h1>
-        <p className="mx-auto text-base leading-relaxed lg:w-2/3">
-          No matter if you're a SaaS, personal brand, service agency or podcast
-          website - our voice widgets add value to everyone.
-        </p>
-      </div> */}
-      <div className="container flex flex-col items-center px-5 mx-auto md:flex-row md:items-center">
-        <div className="flex flex-col items-center mb-16 text-center lg:flex-grow md:w-1/2 md:pr-6 md:items-start md:text-left md:mb-0">
-          <h1 className="mb-4 text-xl font-semibold text-gray-900 title-font sm:text-5xl ">
-            Welcome your audience
-          </h1>
-          <p className="mb-8 leading-relaxed">
-            As soon as someone enters your page, tell them a quick nice hello
-            and make them feel appreciated. You can also introduce yourself. :)
-          </p>
-          <nav className="flex flex-col sm:items-start sm:text-left text-center items-center -mb-1 space-y-2.5">
-            <a>
-              <span className="inline-flex items-center justify-center w-4 h-4 mr-2 text-indigo-500 bg-indigo-100 rounded-full">
-                <svg
-                  fill="none"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={3}
-                  className="w-3 h-3"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M20 6L9 17l-5-5" />
-                </svg>
-              </span>
-              Builds trust
-            </a>
-            <a>
-              <span className="inline-flex items-center justify-center w-4 h-4 mr-2 text-indigo-500 bg-indigo-100 rounded-full">
-                <svg
-                  fill="none"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={3}
-                  className="w-3 h-3"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M20 6L9 17l-5-5" />
-                </svg>
-              </span>
-              Establishes a personal connection between you and your users
-            </a>
-            <a>
-              <span className="inline-flex items-center justify-center w-4 h-4 mr-2 text-indigo-500 bg-indigo-100 rounded-full">
-                <svg
-                  fill="none"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={3}
-                  className="w-3 h-3"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M20 6L9 17l-5-5" />
-                </svg>
-              </span>
-              Makes your site stand out amongst competition
-            </a>
-            <a></a>
-            <a></a>
-          </nav>
-        </div>
-        <div className="w-5/6 lg:max-w-lg lg:w-2/5 md:w-1/2"></div>
-      </div>
       <div className="container flex flex-col flex-col-reverse items-center px-5 pt-24 mx-auto md:flex-row md:items-start">
         <div className="w-5/6 lg:max-w-lg lg:w-2/5 md:w-1/2">
           <img
@@ -84,8 +11,8 @@ const Usecases: React.FunctionComponent = () => {
           />
         </div>
         <div className="flex flex-col items-center mb-16 text-center lg:flex-grow md:w-1/2 md:pl-6 md:items-start md:text-left md:mb-0">
-          <h1 className="mb-4 text-xl font-semibold text-gray-900 title-font sm:text-5xl ">
-            Explain your product or service
+          <h1 className="mb-4 text-xl font-semibold text-gray-900 title-font sm:text-4xl ">
+            Use speak voicys to explain your product
           </h1>
           <p className="mb-8 leading-relaxed">
             Sometimes a good copy just isn't enough to make people understand
@@ -146,7 +73,145 @@ const Usecases: React.FunctionComponent = () => {
           </nav>
         </div>
       </div>
-      <div className="container flex flex-col items-center px-5 pt-24 mx-auto md:flex-row md:items-center">
+
+      <div className="container flex flex-col items-center px-5 mx-auto mt-40 md:flex-row md:items-center">
+        <div className="flex flex-col items-center mb-16 text-center lg:flex-grow md:w-1/2 md:pr-6 md:items-start md:text-left md:mb-0">
+          <h1 className="mb-4 text-xl font-semibold text-gray-900 title-font sm:text-3xl ">
+            Use Listen voicys to collect customer feedback.
+          </h1>
+          <p className="mb-8 leading-relaxed">
+            As soon as someone enters your page, tell them a quick nice hello
+            and make them feel appreciated. You can also introduce yourself. :)
+          </p>
+          <nav className="flex flex-col sm:items-start sm:text-left text-center items-center -mb-1 space-y-2.5">
+            <a>
+              <span className="inline-flex items-center justify-center w-4 h-4 mr-2 text-indigo-500 bg-indigo-100 rounded-full">
+                <svg
+                  fill="none"
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={3}
+                  className="w-3 h-3"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M20 6L9 17l-5-5" />
+                </svg>
+              </span>
+              Builds trust
+            </a>
+            <a>
+              <span className="inline-flex items-center justify-center w-4 h-4 mr-2 text-indigo-500 bg-indigo-100 rounded-full">
+                <svg
+                  fill="none"
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={3}
+                  className="w-3 h-3"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M20 6L9 17l-5-5" />
+                </svg>
+              </span>
+              Establishes a personal connection between you and your users
+            </a>
+            <a>
+              <span className="inline-flex items-center justify-center w-4 h-4 mr-2 text-indigo-500 bg-indigo-100 rounded-full">
+                <svg
+                  fill="none"
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={3}
+                  className="w-3 h-3"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M20 6L9 17l-5-5" />
+                </svg>
+              </span>
+              Makes your site stand out amongst competition
+            </a>
+            <a></a>
+            <a></a>
+          </nav>
+        </div>
+        <div className="w-5/6 lg:max-w-lg lg:w-2/5 md:w-1/2"></div>
+      </div>
+
+      <div className="container flex flex-col flex-col-reverse items-center px-5 pt-24 mx-auto mt-40 md:flex-row md:items-start">
+        <div className="w-5/6 lg:max-w-lg lg:w-1/2 md:w-1/2">
+          <img
+            className="object-cover object-center border border-gray-200 rounded-xl"
+            alt="hero"
+            src="/images/graph.png"
+          />
+        </div>
+        <div className="flex flex-col items-center mb-16 text-center lg:flex-grow md:w-1/2 md:pl-10 md:items-start md:text-left md:mb-0">
+          <h1 className="mb-4 text-xl font-semibold text-gray-900 title-font sm:text-4xl ">
+            Track customer interaction in real-time.
+          </h1>
+          <p className="mb-8 leading-relaxed">
+            Sometimes a good copy just isn't enough to make people understand
+            what it is you do. Instead, simply TELL them what you're all about -
+            in your own words, using your own voice.
+          </p>
+          <nav className="flex flex-col sm:items-start sm:text-left text-center items-center -mb-1 space-y-2.5">
+            <a>
+              <span className="inline-flex items-center justify-center w-4 h-4 mr-2 text-indigo-500 bg-indigo-100 rounded-full">
+                <svg
+                  fill="none"
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={3}
+                  className="w-3 h-3"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M20 6L9 17l-5-5" />
+                </svg>
+              </span>
+              Creates trust
+            </a>
+            <a>
+              <span className="inline-flex items-center justify-center w-4 h-4 mr-2 text-indigo-500 bg-indigo-100 rounded-full">
+                <svg
+                  fill="none"
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={3}
+                  className="w-3 h-3"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M20 6L9 17l-5-5" />
+                </svg>
+              </span>
+              Increases conversions
+            </a>
+            <a>
+              <span className="inline-flex items-center justify-center w-4 h-4 mr-2 text-indigo-500 bg-indigo-100 rounded-full">
+                <svg
+                  fill="none"
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={3}
+                  className="w-3 h-3"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M20 6L9 17l-5-5" />
+                </svg>
+              </span>
+              Decreases churn rate
+            </a>
+            <a></a>
+            <a></a>
+          </nav>
+        </div>
+      </div>
+
+      {/* <div className="container flex flex-col items-center px-5 pt-24 mx-auto md:flex-row md:items-center">
         <div className="flex flex-col items-center mb-16 text-center lg:flex-grow md:w-1/2 md:pr-6 md:items-start md:text-left md:mb-0">
           <h1 className="mb-4 text-xl font-semibold text-gray-900 title-font sm:text-5xl ">
             Use vocal CTA's
@@ -220,8 +285,8 @@ const Usecases: React.FunctionComponent = () => {
             src="/vocal_cts_2.svg"
           />
         </div>
-      </div>
-      <div className="container flex flex-col flex-col-reverse items-center px-5 pt-24 mx-auto md:flex-row md:items-center">
+      </div> */}
+      {/* <div className="container flex flex-col flex-col-reverse items-center px-5 pt-24 mx-auto md:flex-row md:items-center">
         <div className="w-5/6 lg:max-w-lg lg:w-2/5 md:w-1/2">
           <img
             className="object-cover object-center rounded"
@@ -291,7 +356,7 @@ const Usecases: React.FunctionComponent = () => {
             <a></a>
           </nav>
         </div>
-      </div>
+      </div> */}
     </section>
   );
 };
