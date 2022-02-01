f51697151465b6247122e0363e6de5308009235f
Zargham Khan <zarghamkhan980@gmail.com>
2022-02-01 22:52:42 +0100

    Added smart embed section

--- components/Usecases.tsx
+++ components/Usecases.tsx
@@ -77,7 +77,7 @@ const Usecases: React.FunctionComponent = () => {
 
       <div className="container flex flex-col items-center px-5 mx-auto mt-40 md:flex-row md:items-center">
         <div className="flex flex-col items-center mb-16 text-center lg:flex-grow md:w-1/2 md:pr-6 md:items-start md:text-left md:mb-0">
-          <h1 className="mb-4 text-xl font-semibold tracking-tighter text-gray-900 title-font sm:text-3xl ">
+          <h1 className="mb-4 text-xl font-semibold tracking-tighter text-gray-900 title-font sm:text-4xl ">
             Use Listen voicys to collect customer feedback
           </h1>
           <p className="mb-8 leading-relaxed tracking-tighter">
@@ -150,12 +150,78 @@ const Usecases: React.FunctionComponent = () => {
       <div className="container flex flex-col flex-col-reverse items-center px-5 pt-24 mx-auto mt-40 md:flex-row md:items-start">
         <div className="w-5/6 lg:max-w-lg lg:w-1/2 md:w-1/2">
           <img
-            className="object-cover object-center border border-gray-200 rounded-xl"
+            className="object-cover object-center rounded"
             alt="hero"
-            src="/images/graph.png"
+            src="/images/smart_embed.gif"
           />
         </div>
-        <div className="flex flex-col items-center mb-16 text-center lg:flex-grow md:w-1/2 md:pl-10 md:items-start md:text-left md:mb-0">
+        <div className="flex flex-col items-center mb-16 text-center lg:flex-grow md:w-1/2 md:pl-6 md:items-start md:text-left md:mb-0">
+          <h1 className="mb-4 text-xl font-semibold tracking-tighter text-gray-900 title-font sm:text-4xl ">
+            Use Smart Embed, a true no code solution.
+          </h1>
+          <p className="mb-8 leading-relaxed tracking-tighter">
+            You will need to add our script only once. Our no-code technology
+            enables you place voicys anywhere on your website without making any
+            further changes.
+          </p>
+
+          <nav className="flex flex-col sm:items-start sm:text-left text-center items-center -mb-1 space-y-2.5">
+            <a className="text-sm">
+              <span className="inline-flex items-center justify-center w-4 h-4 mr-2 rounded-full text-our-800 bg-our-300">
+                <svg
+                  fill="none"
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={3}
+                  className="w-3 h-3"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M20 6L9 17l-5-5" />
+                </svg>
+              </span>
+              No-code approach
+            </a>
+            <a className="text-sm">
+              <span className="inline-flex items-center justify-center w-4 h-4 mr-2 rounded-full text-our-800 bg-our-300">
+                <svg
+                  fill="none"
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={3}
+                  className="w-3 h-3"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M20 6L9 17l-5-5" />
+                </svg>
+              </span>
+              Easy to integrate
+            </a>
+            <a className="text-sm">
+              <span className="inline-flex items-center justify-center w-4 h-4 mr-2 rounded-full text-our-800 bg-our-300">
+                <svg
+                  fill="none"
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={3}
+                  className="w-3 h-3"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M20 6L9 17l-5-5" />
+                </svg>
+              </span>
+              Only one line of script
+            </a>
+            <a></a>
+            <a></a>
+          </nav>
+        </div>
+      </div>
+
+      <div className="container flex flex-col flex-col-reverse items-center px-5 pt-24 mx-auto mt-40 md:flex-row md:items-start">
+        <div className="flex flex-col items-center mb-16 text-center lg:flex-grow md:w-1/2 md:items-start md:text-left md:mb-0">
           <h1 className="mb-4 text-xl font-semibold tracking-tighter text-gray-900 title-font sm:text-4xl ">
             Track customer interaction in real-time
           </h1>
@@ -217,6 +283,13 @@ const Usecases: React.FunctionComponent = () => {
             <a></a>
             <a></a>
           </nav>
+        </div>
+        <div className="w-5/6 lg:max-w-lg lg:w-1/2 md:w-1/2 md:pl-10">
+          <img
+            className="object-cover object-center border border-gray-200 rounded-xl"
+            alt="hero"
+            src="/images/graph.png"
+          />
         </div>
       </div>
 
