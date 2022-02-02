c5ebe116106cc17fd75076aafd22426fc8a25d03
svenjarossig <87528035+svenjarossig@users.noreply.github.com>
2022-02-02 13:41:30 +0000

    listen

---
+++ pages/listen.tsx
@@ -0,0 +1,201 @@
+import Layout from "../components/Layout";
+import Typist from "react-typist";
+
+export default function Faqs() {
+  return (
+    <Layout>
+        
+
+{/* Section 1 */}
+
+
+<div className="flex flex-col w-full mt-20 text-center">
+          <h1 className="box-border m-0 text-3xl font-semibold leading-tight tracking-tighter text-black border-solid sm:text-4xl md:text-5xl">
+            Receive spoken feedback
+          </h1>
+          <p className="box-border mt-2 tracking-tighter text-xl font-medium text-gray-900 border-solid sm:text-2xl">
+            Make better decisions by asking your audience for their feedback directly.
+          </p>
+        </div>
+
+        <div className="container rounded-3xl flex bg-our-300 flex-col items-center px-5 mx-auto mt-40 md:flex-row md:items-center">
+        <div className="flex flex-col items-center mb-16 text-center lg:flex-grow md:w-1/2 md:pr-6 md:items-start md:text-left md:mb-0">
+          <h1 className="mb-4 mt-20 text-xl font-semibold tracking-tighter text-our-800 title-font sm:text-4xl ">
+            Use Listen voicys to collect customer feedback
+          </h1>
+          <p className="mb-8 text-our-800 leading-relaxed tracking-tighter">
+            Ask your audience for their opinion directly. Plus, people will
+            leave feedback more frequently because it doesn't cost them time
+            typing it down anymore.
+          </p>
+          <nav className="flex flex-col sm:items-start sm:text-left text-center items-center -mb-1 space-y-2.5">
+            <a className="text-sm tracking-tighter text-our-800">
+              <span className="inline-flex items-center justify-center w-4 h-4 mr-2 rounded-full text-our-800 bg-white">
+                <svg
+                  fill="none"
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={3}
+                  className="w-3 h-3"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M20 6L9 17l-5-5" />
+                </svg>
+              </span>
+              VALIDATE & IMPROVE YOUR PRODUCT
+            </a>
+            <a className="text-sm tracking-tighter text-our-800">
+              <span className="inline-flex items-center justify-center w-4 h-4 mr-2 rounded-full text-our-800 bg-white">
+                <svg
+                  fill="none"
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={3}
+                  className="w-3 h-3"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M20 6L9 17l-5-5" />
+                </svg>
+              </span>
+              RECEIVE DIRECT FEEDBACK
+            </a>
+            <a className="text-sm tracking-tighter text-our-800">
+              <span className="inline-flex mb-20 items-center justify-center w-4 h-4 mr-2 rounded-full text-our-800 bg-white">
+                <svg
+                  fill="none"
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={3}
+                  className="w-3 h-3"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M20 6L9 17l-5-5" />
+                </svg>
+              </span>
+              INVOLVE YOUR AUDIENCE
+            </a>
+            <a></a>
+            <a></a>
+          </nav>
+        </div>
+        <div className="w-5/6 lg:max-w-lg lg:w-2/5 md:w-1/2">
+          <img
+            className="object-cover object-center border border-gray-200 rounded-xl"
+            alt="hero"
+            src="/images/listen.gif"
+          />
+        </div>
+      </div>
+
+      <div className="container rounded-3xl flex bg-our-300 flex-col items-center px-5 mx-auto mt-20 md:flex-row md:items-center">
+        <div className="flex flex-col items-center mb-16 text-center lg:flex-grow md:w-1/2 md:pr-6 md:items-start md:text-left md:mb-0">
+          <h1 className="mb-4 mt-20 text-xl font-semibold tracking-tighter text-our-800 title-font sm:text-4xl ">
+            Use Listen voicys to collect customer feedback
+          </h1>
+          <p className="mb-8 text-our-800 leading-relaxed tracking-tighter">
+            Ask your audience for their opinion directly. Plus, people will
+            leave feedback more frequently because it doesn't cost them time
+            typing it down anymore.
+          </p>
+          <nav className="flex flex-col sm:items-start sm:text-left text-center items-center -mb-1 space-y-2.5">
+            <a className="text-sm tracking-tighter text-our-800">
+              <span className="inline-flex items-center justify-center w-4 h-4 mr-2 rounded-full text-our-800 bg-white">
+                <svg
+                  fill="none"
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={3}
+                  className="w-3 h-3"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M20 6L9 17l-5-5" />
+                </svg>
+              </span>
+              VALIDATE & IMPROVE YOUR PRODUCT
+            </a>
+            <a className="text-sm tracking-tighter text-our-800">
+              <span className="inline-flex items-center justify-center w-4 h-4 mr-2 rounded-full text-our-800 bg-white">
+                <svg
+                  fill="none"
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={3}
+                  className="w-3 h-3"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M20 6L9 17l-5-5" />
+                </svg>
+              </span>
+              RECEIVE DIRECT FEEDBACK
+            </a>
+            <a className="text-sm tracking-tighter text-our-800">
+              <span className="inline-flex mb-20 items-center justify-center w-4 h-4 mr-2 rounded-full text-our-800 bg-white">
+                <svg
+                  fill="none"
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={3}
+                  className="w-3 h-3"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M20 6L9 17l-5-5" />
+                </svg>
+              </span>
+              INVOLVE YOUR AUDIENCE
+            </a>
+            <a></a>
+            <a></a>
+          </nav>
+        </div>
+        <div className="w-5/6 lg:max-w-lg lg:w-2/5 md:w-1/2">
+          <img
+            className="object-cover object-center border border-gray-200 rounded-xl"
+            alt="hero"
+            src="/images/listen.gif"
+          />
+        </div>
+      </div>
+      
+      <div className="flex flex-col w-full mt-20 text-center">
+          <h1 className="box-border m-0 text-3xl font-semibold leading-tight tracking-tighter text-black border-solid sm:text-4xl md:text-5xl">
+Meet our widgets          </h1>
+          
+        </div>
+
+        <div className="mx-4 mt-40 bg-[#F6F3FB] rounded-3xl text-our-800">
+      <div className="flex flex-col w-full px-4 py-12 mx-auto rounded-3xl sm:px-6 lg:py-16 lg:px-16">
+        <h2 className="text-3xl font-bold sm:text-4xl">Try Voicl for free</h2>
+        <p className="mt-3 text-our-700">
+          Getting started is easy and takes a couple of minutes only. No credit
+          card required. No hidden fees.
+        </p>
+        <div className="flex flex-wrap mt-10 -mx-2">
+        <a href="https://calendly.com/svenjarossig/voicl-demo" className="items-center block px-10 py-3 mx-2 text-sm font-medium text-center transition duration-500 ease-in-out transform border border-our-500 text-our-700 rounded-xl focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-our-500">Book a demo</a> 
+            
+          <button className="mt-2 sm:mt-0 text-sm whitespace-nowrap mx-2 items-center block px-10 py-3 font-medium text-center text-white transition duration-500 ease-in-out transform bg-gradient-to-r from-[#a994e8] to-[#886adb] rounded-xl  focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+            Start Now - its free
+          </button>
+        </div>
+      </div>
+    </div>
+
+        
+
+      
+
+     
+
+
+
+
+
+
+    </Layout>
+  );
+}
