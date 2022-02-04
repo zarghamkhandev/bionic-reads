aedefece81f2e0bee9ec1178001d83f8ee8c6599
Zargham Khan <zarghamkhan980@gmail.com>
2022-02-04 23:36:27 +0100

    pushed

--- components/CTA.tsx
+++ components/CTA.tsx
@@ -4,17 +4,27 @@ const CTA: FunctionComponent = () => {
   return (
     <div className="mx-4 mt-40 bg-[#F6F3FB] rounded-3xl text-our-800">
       <div className="flex flex-col w-full px-4 py-12 mx-auto rounded-3xl sm:px-6 lg:py-16 lg:px-16">
-        <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl">Try Voicl for free</h2>
+        <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl">
+          Try Voicl for free
+        </h2>
         <p className="mt-3 tracking-tighter text-our-700">
           Getting started is easy and takes a couple of minutes only. No credit
           card required. No hidden fees.
         </p>
         <div className="flex flex-wrap mt-10 -mx-2">
-        <a href="https://calendly.com/svenjarossig/voicl-demo" className="items-center block px-10 py-3 mx-2 text-sm font-medium text-center transition duration-500 ease-in-out transform border border-our-500 text-our-700 rounded-xl focus:outline-none focus:ring-2 focus:ring-offset-2 tracking-tighter focus:ring-our-500">Book a demo</a> 
-            
-          <button className="mt-2 tracking-tighter sm:mt-0 text-sm whitespace-nowrap mx-2 items-center block px-10 py-3 font-medium text-center text-white transition duration-500 ease-in-out transform bg-gradient-to-r from-[#a994e8] to-[#886adb] rounded-xl  focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+          <a
+            href="https://calendly.com/svenjarossig/voicl-demo"
+            className="items-center block px-10 py-3 mx-2 text-sm font-medium tracking-tighter text-center transition duration-500 ease-in-out transform border border-our-500 text-our-700 rounded-xl focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-our-500"
+          >
+            Book a demo
+          </a>
+
+          <a
+            className="mt-2 tracking-tighter sm:mt-0 text-sm whitespace-nowrap mx-2 items-center block px-10 py-3 font-medium text-center text-white transition duration-500 ease-in-out transform bg-gradient-to-r from-[#a994e8] to-[#886adb] rounded-xl  focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+            href="https://app.voicl.com/login"
+          >
             Start Now - its free
-          </button>
+          </a>
         </div>
       </div>
     </div>
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -21,7 +21,7 @@ const Hero: React.FunctionComponent<HeroProps> = ({}) => {
         <div className="flex w-full mx-auto text-left">
           <div className="relative inline-flex items-center mx-auto align-middle">
             <div className="text-center">
-              <h1 className="w-full text-3xl tracking-tighter font-bold text-grey-100 md:text-5xl lg:text-6xl text-slate-700">
+              <h1 className="w-full text-3xl font-bold tracking-tighter text-grey-100 md:text-5xl lg:text-6xl text-slate-700">
                 Did you know that your <br></br>website can{" "}
                 <Typist
                   className="text-transparent bg-clip-text bg-gradient-to-br from-[#cb5eee] to-[#3085f0] ml-4"
@@ -31,16 +31,19 @@ const Hero: React.FunctionComponent<HeroProps> = ({}) => {
                 </Typist>
               </h1>
 
-              <p className="max-w-xl tracking-tighter mx-auto mt-8 text-xl leading-relaxed text-gray-500 ">
+              <p className="max-w-xl mx-auto mt-8 text-xl leading-relaxed tracking-tighter text-gray-500 ">
                 Use voice widgets to speak to your audience & collect voice
                 feedback. Increase conversions and make better decisions.
               </p>
 
               <div className="flex justify-center w-full max-w-2xl gap-2 mx-auto mt-6">
                 <div className="mt-3 rounded-lg sm:mt-0">
-                  <button className="items-center tracking-tighter block px-5 py-4 text-base font-normal text-center text-white transition duration-500 ease-in-out transform bg-gradient-to-r from-[#a994e8] to-[#886adb] hover:bg-[#F6F3FB] text-white  lg:px-10 rounded-xl focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+                  <a
+                    className="items-center tracking-tighter block px-5 py-4 text-base font-normal text-center text-white transition duration-500 ease-in-out transform bg-gradient-to-r from-[#a994e8] to-[#886adb] hover:bg-[#F6F3FB] text-white  lg:px-10 rounded-xl focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                    href="https://app.voicl.com/login"
+                  >
                     Start now - it's free
-                  </button>
+                  </a>
                 </div>
               </div>
             </div>
