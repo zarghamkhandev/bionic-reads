87ef661f56b9651c8ae920f5f5216651e266b097
Zargham Khan <zarghamkhan980@gmail.com>
2022-02-06 17:10:31 +0100

    replace endpoint

--- helpers/backend.ts
+++ helpers/backend.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { WPPost } from "../types";
-const wordpressUrl = "http://3.121.109.114/wp-json/wp/v2";
+const wordpressUrl = "https://wp.voicl.com/wp-json/wp/v2";
 export const getAllPosts = async () => {
   return (await axios.get(wordpressUrl + "/posts/")).data as WPPost[];
 };
